aefb598fa888ab2657dfe0342e0fdaafd4322481
test <test@gmail.com>
2025-04-05 15:36:48 +0530

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,39 @@
+﻿using BlogApp.Models;
+using BlogApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly BlogService _blogService;
+
+        public BlogController(BlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IActionResult Index()
+        {
+            var blogs = _blogService.GetAllBlogs();
+            return View(blogs);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _blogService.AddBlog(blog);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(blog);
+        }
+    }
+}
---
+++ Services/BlogService.cs
@@ -0,0 +1,35 @@
+﻿using BlogApp.Models;
+using System.Text.Json;
+
+namespace BlogApp.Services
+{
+    public class BlogService
+    {
+        private const string FilePath = "blogs.json";
+        public List<Blog> GetAllBlogs()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return new List<Blog>();
+            }
+
+            var json = File.ReadAllText(FilePath);
+            return JsonSerializer.Deserialize<List<Blog>>(json) ?? new List<Blog>();
+        }
+
+        public void AddBlog(Blog blog)
+        {
+            var blogs = GetAllBlogs();
+            blog.Id = blogs.Any() ? blogs.Max(b => b.Id) + 1 : 1;
+            blog.CreatedAt = DateTime.UtcNow;
+            blogs.Add(blog);
+            SaveBlogsToFile(blogs);
+        }
+
+        private void SaveBlogsToFile(List<Blog> blogs)
+        {
+            var json = JsonSerializer.Serialize(blogs, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePath, json);
+        }
+    }
+}
